c93d0fd836ab471ffd7898904227cfa317f2e90d
luistaffelli <luistaffelli@gmail.com>
2022-12-20 16:10:32 -0300

    gh-pages

--- src/Pages/Landing/Sections/index.jsx
+++ src/Pages/Landing/Sections/index.jsx
@@ -2,10 +2,12 @@ import TopSection from './TopSection';
 import UsSection from './UsSection';
 import PortfolioSection from './PortfolioSection';
 import OfferSection from './OfferSection';
+import BottomSection from './BottomSection';
 
 export {
 	TopSection,
 	UsSection,
 	PortfolioSection,
-	OfferSection
+	OfferSection,
+	BottomSection
 }
