005b5a6404870b205a67379f13e034b55d944291
luistaffelli <luistaffelli@gmail.com>
2023-03-10 13:51:04 -0300

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Landing from './Pages/Landing/Landing';
 import ComingSoon from './Pages/ComingSoon/';
 import Footer from './GeneralComponents/Footer/';
 import TopFooter from './GeneralComponents/FooterTop/SMFooter'
-import Planes from './Pages/Planes/index';
+import Planes from './Pages/Planes/Index';
 
 function App() {
 
