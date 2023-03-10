4405de7871325c6d794862b487b584f9057ef600
luistaffelli <luistaffelli@gmail.com>
2023-03-10 13:30:49 -0300

    Fix for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.scss';
 import './App.css';
 import Navbar from './GeneralComponents/NavBar/index';
 import Landing from './Pages/Landing/Landing';
-import Planes from './Pages/Planes/';
+// import Planes from './Pages/Planes/';
 import ComingSoon from './Pages/ComingSoon/';
 import Footer from './GeneralComponents/Footer/';
 import TopFooter from './GeneralComponents/FooterTop/SMFooter'
@@ -38,7 +38,7 @@ function App() {
           
           <Route element={<Landing ScrollTo={Section}/>} exact path="/"/>
 
-          <Route element={<Planes />} exact path="/Planes"/>
+          {/*<Route element={<Planes />} exact path="/Planes"/>*/}
 
           <Route element={<Navigate to="/" />} path="*" />
         </Routes>
