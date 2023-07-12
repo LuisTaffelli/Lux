37f2234ef98cb4e736049fc13de989315434e891
Luis <86675342+LuisTaffelli@users.noreply.github.com>
2023-07-12 13:11:27 -0300

    Update WhatsappLink.js

--- src/Utils/WhatsappLink.js
+++ src/Utils/WhatsappLink.js
@@ -5,9 +5,9 @@
 export default function WhatsappLink(Text){
 
 	if(!Text){
-		return `https://api.whatsapp.com/send?phone=5491162471651&text=Hola! Quiero conocer más acerca de su empresa!`		
+		return `https://api.whatsapp.com/send?phone=5491155938576&text=Hola! Quiero conocer más acerca de su empresa!`		
 	}
 
 
-	return `https://api.whatsapp.com/send?phone=5491162471651&text=${Text}`
+	return `https://api.whatsapp.com/send?phone=5491155938576&text=${Text}`
 }
